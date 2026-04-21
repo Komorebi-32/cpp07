9dd02cd02d3ba92ef2c213244e4f79eef8703437
michel_32 <karas.benjamin@gmail.com>
2026-04-21 11:55:23 +0200

    ex01: implement iter() with data and function pointer typenames + add docstrings

--- ex01/inc/iter.hpp
+++ ex01/inc/iter.hpp
@@ -6,12 +6,23 @@
 /*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 17:55:25 by michel_32         #+#    #+#             */
-/*   Updated: 2026/04/20 18:01:47 by michel_32        ###   ########.fr       */
+/*   Updated: 2026/04/21 11:53:23 by michel_32        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-template<typename T>
-void iter(T *array, const unsigned int length, void (*func)(T &))
+/*
+typename D == Data Type. It represents the type of the elements stored in the array.
+typename F == Function pointer type. It represents the entire signature of a function. 
+
+No need to specify the number of arguments in `F func` because the compiler 
+deduces the number of arguments of func() in the code: `func(array[i]);`
+
+Defining a function pointer type allows to handle both cases when the argument 
+of `func()` is `const` and when it's `non-const`
+*/
+template<typename D, typename F>
+void iter(D *array, const unsigned int length, F func)
 {
-    
+    for (int i = 0; i < length; i++)
+        func(array[i]);
 }
