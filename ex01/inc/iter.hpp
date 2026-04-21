ad6051af8dd1c53f64619ee0dcc62a8e88cf975d
michel_32 <karas.benjamin@gmail.com>
2026-04-21 14:38:14 +0200

    add main + fix compile error: switch int to unsigned int type in for loop of iter()

--- ex01/inc/iter.hpp
+++ ex01/inc/iter.hpp
@@ -6,9 +6,12 @@
 /*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 17:55:25 by michel_32         #+#    #+#             */
-/*   Updated: 2026/04/21 11:53:23 by michel_32        ###   ########.fr       */
+/*   Updated: 2026/04/21 14:34:19 by michel_32        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#define MAGENTA "\033[35m"
+#define RESET   "\033[0m"
 
 /*
 typename D == Data Type. It represents the type of the elements stored in the array.
@@ -23,6 +26,6 @@ of `func()` is `const` and when it's `non-const`
 template<typename D, typename F>
 void iter(D *array, const unsigned int length, F func)
 {
-    for (int i = 0; i < length; i++)
+    for (unsigned int i = 0; i < length; i++)
         func(array[i]);
 }
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,8 +6,36 @@
 /*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 17:55:34 by michel_32         #+#    #+#             */
-/*   Updated: 2026/04/20 17:57:09 by michel_32        ###   ########.fr       */
+/*   Updated: 2026/04/21 14:36:44 by michel_32        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+#include "iter.hpp"
 
+void print(const int &i)
+{
+    std::cout << i << std::endl;
+}
+
+void increment32(int &i)
+{
+    i+=32;
+}
+
+int main(void)
+{
+    int tab[] = {2, 4, 8, 16, 32, 32, 32, 32};
+    unsigned int length = 8;
+
+    std::cout << MAGENTA << "\n-----------initial tab----------" << RESET << std::endl;
+    iter(tab, length, print);
+    std::cout << "\n";
+    
+    iter(tab, length, increment32);
+
+    std::cout << MAGENTA << "\n-----------incremented tab----------" << RESET << std::endl;
+    iter(tab, length, print);
+    std::cout << "\n";
+}
