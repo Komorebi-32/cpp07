54a44af944c73aac25b0898847eb8107a6a74943
bkaras-g <bkaras-g@student.42.fr>
2026-05-27 16:15:00 +0200

    ex00 main refactor

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
+/*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 17:32:27 by michel_32         #+#    #+#             */
-/*   Updated: 2026/04/20 17:53:04 by michel_32        ###   ########.fr       */
+/*   Updated: 2026/05/27 16:13:33 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,20 @@ int	main(void)
 {
 	int a = 2;
 	int b = 3;
-	::swap(a, b);
+	std::cout << "\nBefore swapping mushrooms" << std::endl;
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	::swap(a, b); //to force the call to the swap() in the global namespace
+	//from mushrooms.hpp
+	std::cout << "\nAfter swapping mushrooms" << std::endl;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "\nmin( a, b ) = " << ::min(a, b) << std::endl;
 	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
+	
+	std::cout << "\nc = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
 	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
 	return (0);
